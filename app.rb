2d0922b2bbb28a011fda0fd4c24d14a760a60069
lieby weintraub <liebahinda@gmail.com>
2019-05-26 07:01:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ get '/square/:number' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
     phrase=[]
-    @number.times do 
+    @number.times { |x| phrase << @phrase} 
     
   end 
 end
